661939b95b756950a408b7cec769a48246e68c0d
jcLucio <c.lucio3@gmail.com>
2021-08-12 19:44:24 -0500

    Add user model

---
+++ models/user.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    username: { type: String, default: '' },
+    name: { type: String, default: '' },
+  },
+  { timestamps: true }
+);
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+    delete returnedObject.passwordHash;
+  },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
