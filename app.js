b6bf4393ecebc5b6b0294c3b1c2798a217f2f0ce
jcLucio <c.lucio3@gmail.com>
2021-08-12 20:58:26 -0500

    Add usersRouter w/ GET/POST routes - Install bcrypt - Update app.js to use usersRouter/ json - Add GET route to get all users - Add POST route to create user - Update toJSON in userSchema to delete returnedObject.password - Add request get_users / create_user

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const { MONGODB_URI } = require('./utils/config');
 const logger = require('./utils/logger');
+const usersRouter = require('./controllers/users');
 
 const app = express();
 
@@ -19,6 +20,10 @@ mongoose
   .catch((error) => {
     logger.error('error connecting to mongoDB', error);
   });
+
+app.use(express.json());
+
+app.use('/api/users', usersRouter);
 
 app.get('/ping', (_, res) => {
   res.send('pong');
