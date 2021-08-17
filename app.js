aac53ba60f03fd8f9072738fa4df0642d4ace97c
jcLucio <c.lucio3@gmail.com>
2021-08-17 00:09:05 -0500

    Add middlewares to app - Add tokenExtactor middleware - Add userExtractor middleware - Add unknownEndpoint middleware

--- app.js
+++ app.js
@@ -2,19 +2,27 @@ const express = require('express');
 const connectDB = require('./db');
 const usersRouter = require('./controllers/users');
 const loginRouter = require('./controllers/login');
+const middleware = require('./middleware');
 
 const app = express();
 
 /**Connect to mongoDB */
 connectDB();
 
+/** Middlewares */
 app.use(express.json());
+app.use(middleware.tokenExtractor);
+app.use(middleware.userExtractor);
 
+/** Routes */
 app.use('/api/users', usersRouter);
 app.use('/api/login', loginRouter);
 
+/** Healthchecker */
 app.get('/ping', (_, res) => {
   res.send('pong');
 });
+
+app.use(middleware.unknownEndpoint);
 
 module.exports = app;
