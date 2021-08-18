9faaa4158bda72fc5728b4abf7835c7467150ef3
jcLucio <c.lucio3@gmail.com>
2021-08-17 19:59:39 -0500

    Fix login route - Change JWT_TOKEN to JWT_SECRET - Add missing await when User.findOne

--- controllers/login.js
+++ controllers/login.js
@@ -1,18 +1,20 @@
 const router = require('express').Router();
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const { JWT_TOKEN } = require('../utils/config');
+const { JWT_SECRET } = require('../utils/config');
 const User = require('../models/user');
 
 router.post('/', async (req, res) => {
   const body = req.body;
 
-  const user = User.find({ email: body.email });
+  const user = await User.findOne({ email: body.email });
 
-  const isPasswordCorrect = await bcrypt.compare(body.password, user.password);
+  const isPasswordCorrect = !user
+    ? false
+    : await bcrypt.compare(body.password, user.password);
 
   if (!(user && isPasswordCorrect)) {
-    return res.status(404).json({ error: 'incorrect email or password' });
+    return res.status(401).json({ error: 'incorrect email or password' });
   }
 
   const userForToken = {
@@ -20,7 +22,7 @@ router.post('/', async (req, res) => {
     id: user._id,
   };
 
-  const token = jwt.sign(userForToken, JWT_TOKEN);
+  const token = jwt.sign(userForToken, JWT_SECRET);
 
   res.status(200).json({ token, username: user.username, name: user.name });
 });
