e3422386eeb1451d7a9e3ad76459ef3b3bd30b63
jcLucio <c.lucio3@gmail.com>
2021-08-25 23:43:02 -0500

    Add route to get user by username

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -6,4 +6,9 @@ router.get('/', async (_req, res) => {
   res.json(users);
 });
 
+router.get('/:username', async (req, res) => {
+  const user = await User.findOne({ username: req.params.username });
+  res.json(user);
+});
+
 module.exports = router;
