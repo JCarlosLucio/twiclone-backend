e39935fb6d6d7cc8272784c30ba9b6d936151abe
jcLucio <c.lucio3@gmail.com>
2021-09-11 19:04:11 -0500

    Update register validation object to include username

--- src/validations/auth.js
+++ src/validations/auth.js
@@ -2,6 +2,15 @@ const yup = require('yup');
 
 const register = yup.object({
   body: yup.object({
+    username: yup
+      .string()
+      .required()
+      .min(4)
+      .max(15)
+      .matches(
+        /^[a-zA-Z0-9_]+$/,
+        'username must contain only letters, numbers, underscores and no spaces'
+      ),
     email: yup.string().email().required(),
     password: yup.string().required().min(4).max(128),
   }),
