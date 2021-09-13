8e8615657ca0f5ca2567128cbc113a6b731c1510
jcLucio <c.lucio3@gmail.com>
2021-09-12 22:40:18 -0500

    Add minLength to username in user model

--- src/models/user.js
+++ src/models/user.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema(
   {
     email: { type: String, required: true, unique: true },
     password: { type: String, required: true },
-    username: { type: String, required: true, unique: true },
+    username: { type: String, required: true, unique: true, minLength: 5 },
     name: { type: String, required: true },
   },
   { timestamps: true }
