64ef507e6be09368d67b57b0348ba8da203d37f2
jcLucio <c.lucio3@gmail.com>
2021-09-23 23:30:10 -0500

    Update createTweet validations to check parent

--- src/validations/tweets.js
+++ src/validations/tweets.js
@@ -3,6 +3,7 @@ const yup = require('yup');
 const createTweet = yup.object({
   body: yup.object({
     content: yup.string().required().max(280),
+    parent: yup.string().nullable(),
   }),
 });
 
