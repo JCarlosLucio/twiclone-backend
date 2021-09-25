8f51664b541c17a5e9a4a158328d417e534029d9
jcLucio <c.lucio3@gmail.com>
2021-09-24 23:41:23 -0500

    Remove special email/username ValidationError cases from errorHandler

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -9,12 +9,6 @@ const errorHandler = (error, _req, res, next) => {
   if (error.name === 'CastError') {
     return res.status(400).send({ error: 'malformatted id' });
   } else if (error.name === 'ValidationError') {
-    if (error?.errors?.email?.message) {
-      return res.status(400).json({ error: error.errors.email.message });
-    }
-    if (error?.errors?.username?.message) {
-      return res.status(400).json({ error: error.errors.username.message });
-    }
     return res.status(400).json({ error: error.message });
   } else if (error.name === 'JsonWebTokenError') {
     return res.status(401).json({ error: 'invalid token' });
