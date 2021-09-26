da8ded1b61b1d238cf8cf3565f599ed1ffcf570a
jcLucio <c.lucio3@gmail.com>
2021-09-25 21:37:42 -0500

    Update userSchema with bio and location - Add bio property - Add location property

--- src/models/user.js
+++ src/models/user.js
@@ -2,10 +2,36 @@ const mongoose = require('mongoose');
 
 const userSchema = new mongoose.Schema(
   {
-    email: { type: String, required: true, unique: true },
-    password: { type: String, required: true },
-    username: { type: String, required: true, unique: true, minLength: 5 },
-    name: { type: String, required: true },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      minLength: 5,
+    },
+    name: {
+      type: String,
+      required: true,
+      maxLength: 50,
+    },
+    bio: {
+      type: String,
+      maxLength: 160,
+      default: '',
+    },
+    location: {
+      type: String,
+      maxLength: 30,
+      default: '',
+    },
   },
   { timestamps: true }
 );
