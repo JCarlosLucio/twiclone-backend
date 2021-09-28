d2cd090be861b77a133a614c798c540ca637380a
jcLucio <c.lucio3@gmail.com>
2021-09-28 13:32:29 -0500

    Add POST /api/users/:id/follow route to handle follow/unfollow

--- src/controllers/users.js
+++ src/controllers/users.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const User = require('../models/user');
+const { userExtractor } = require('../middleware');
 
 router.get('/', async (_req, res) => {
   const users = await User.find({});
@@ -14,6 +15,36 @@ router.get('/:username', async (req, res) => {
   }
 
   res.json(user);
+});
+
+router.post('/:id/follow', userExtractor, async (req, res) => {
+  const id = req.params.id;
+  const user = req.user;
+  const userFollow = await User.findById(id);
+
+  if (user._id.toString() === userFollow._id.toString()) {
+    return res.status(403).json({ error: 'Cannot follow yourself' });
+  }
+
+  const isFollowing =
+    user.following.includes(userFollow._id) &&
+    userFollow.followers.includes(user._id);
+
+  // if user is following userFollow
+  if (isFollowing) {
+    // Remove userFollow from user's following and user from userFollow's followers
+    user.following.pull(userFollow._id);
+    userFollow.followers.pull(user._id);
+  } else {
+    // Add userFollow to user's following and user to userFollow's followers
+    user.following.push(userFollow._id);
+    userFollow.followers.push(user._id);
+  }
+
+  const updatedMe = await user.save();
+  const updatedUser = await userFollow.save();
+
+  res.status(200).json({ updatedMe, updatedUser });
 });
 
 module.exports = router;
