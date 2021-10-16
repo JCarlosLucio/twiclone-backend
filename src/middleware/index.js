c50a031f59a515431c2be5cf3352c7819344509d
jcLucio <c.lucio3@gmail.com>
2021-10-16 15:17:20 -0500

    Add type to error in errorHandler for ValidationError

--- src/middleware/index.js
+++ src/middleware/index.js
@@ -9,7 +9,7 @@ const errorHandler = (error, _req, res, next) => {
   if (error.name === 'CastError') {
     return res.status(400).send({ error: 'malformatted id' });
   } else if (error.name === 'ValidationError') {
-    return res.status(400).json({ error: error.message });
+    return res.status(400).json({ type: error?.type, error: error.message });
   } else if (error.name === 'JsonWebTokenError') {
     return res.status(401).json({ error: 'invalid token' });
   } else if (error.name === 'TokenExpiredError') {
