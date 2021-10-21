8ddf69acb566ef0e65adf995afd372e975cf4610
jcLucio <c.lucio3@gmail.com>
2021-10-20 20:17:44 -0500

    Add GET tweets/:id/replies route to get a tweet replies

--- src/controllers/tweets.js
+++ src/controllers/tweets.js
@@ -113,4 +113,34 @@ router.put('/:id/like', userExtractor, async (req, res) => {
   res.status(200).json(updatedTweet);
 });
 
+/**
+ * Get Tweet replies paginated
+ */
+
+router.get('/:id/replies', async (req, res) => {
+  const id = req.params.id;
+  const currentPage = Number(req.query.page) || 1;
+  const perPage = Number(req.query.limit) || 10;
+
+  const totalItems = await Tweet.countDocuments({ parent: id });
+  const lastPage = Math.ceil(totalItems / perPage) || 1;
+
+  if (currentPage > lastPage || currentPage <= 0) {
+    return res.status(404).json({ error: 'Page not found.' });
+  }
+
+  const replies = await Tweet.find({ parent: id })
+    .sort({ _id: -1 })
+    .skip((currentPage - 1) * perPage)
+    .limit(perPage)
+    .populate('user', {
+      username: 1,
+      name: 1,
+      bio: 1,
+      avatar: 1,
+    });
+
+  res.status(200).json({ replies, totalItems, currentPage, lastPage });
+});
+
 module.exports = router;
